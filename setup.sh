785a21106594d43f297d2405ec3783d027bfdeb2
Anil <anil@admins-MacBook-Pro-78.local>
2016-04-11 14:41:24 +0530

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 eval $(docker-machine env default)
-docker pull aniryou/spark_bigquery:v1
-docker run -d -p 8888:8888 -v .:/mnt aniryou/spark_bigquery:v1
+docker pull aniryou/spark_bigquery
+docker run -d -p 8888:8888 -v $(pwd):/mnt aniryou/spark_bigquery:v1
 
